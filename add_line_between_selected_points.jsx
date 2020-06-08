c77b7683b7c553e5cab5563b8f2b64f1225a6cf9
nishidate-yuki <asta18425@gmail.com>
2020-06-08 23:29:11 +0900

    update add_line_between_selected_points.jsx

--- add_line_between_selected_points.jsx
+++ add_line_between_selected_points.jsx
@@ -1,3 +1,7 @@
+var doc = app.activeDocument;
+doc.layers.add();   // スクリプトから追加するアイテムは全て新しいレイヤに入れる
+sels = app.activeDocument.selection;
+
 function add_line(pos1, pos2){
     var line = doc.pathItems.add();
 
@@ -12,25 +16,32 @@ function add_line(pos1, pos2){
     line.setEntirePath(Array(pos1, pos2));
 }
 
-var doc = app.activeDocument;
-sels = app.activeDocument.selection;
-if (sels.length > 0){
-    points = sels[0].pathPoints;
+function get_selected_points(selection){
+    // pathItem.selectedPath は余分なポイントが含まれているので
+    // 適当に線形探索で取る関数を作った（よく分からない）
+
+    points = selection.pathPoints;
+    // 選択アイテムがpathItemじゃない場合
+    // pointsがundefinedとなる
     if(points == undefined){
-        alert("please ungroup and uncompoundpath");
+        alert("please ungroup and uncompoundpath!");
     }
-    var list = []
+
+    var selected_points = []
     for (var i = 0; i < points.length; i++) {
         if(points[i].selected == PathPointSelection.ANCHORPOINT){
-            list.push(points[i].anchor)
+            selected_points.push(points[i].anchor)
         }
     }
+    return selected_points;
+}
 
-    if(list.length < 2){
-        alert("too few points");
+if (sels.length > 0){
+    points = get_selected_points(sels[0]);
+    if(points.length < 2){
+        alert("too few points!");
     }
-
-    add_line(list[0], list[1]);
+    add_line(points[0], points[1]);
 }
 
 app.executeMenuCommand('group');
