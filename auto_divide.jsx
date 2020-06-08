3686cf48da40a083e99de5af0677e53b2a24b241
nishidate-yuki <asta18425@gmail.com>
2020-06-08 15:41:26 +0900

    add bidirectional check

--- auto_divide.jsx
+++ auto_divide.jsx
@@ -39,6 +39,22 @@ function dot(a, b){
     return a[0]*b[0] + a[1]*b[1];
 }
 
+function has_left_handle(point){
+    // ハンドル位置とアンカー位置が異なればハンドルを持つ
+    var EPS = 0.0001;
+    var anchor = point.anchor;
+    var left = point.leftDirection;
+    return Math.abs(anchor[0] - left[0]) > EPS || Math.abs(anchor[1] - left[1]) > EPS;
+}
+
+function has_right_handle(point){
+    // ハンドル位置とアンカー位置が異なればハンドルを持つ
+    var EPS = 0.0001;
+    var anchor = point.anchor;
+    var right = point.rightDirection;
+    return Math.abs(anchor[0] - right[0]) > EPS || Math.abs(anchor[1] - right[1]) > EPS;
+}
+
 function calc_cost(sel_id, i, cur_sel_id, j){
 
     base_points = sels[sel_id].pathPoints;
@@ -54,7 +70,8 @@ function calc_cost(sel_id, i, cur_sel_id, j){
     }
 
     // 距離コスト
-    // 何かしらで正規化が必要 -> 道なりに進む場合を基準値にする？
+    // 何かしらで正規化が必要 -> 道なりに進む場合を基準値にする
+    // 隣接する辺でより短い方を採用
     var orig_dist1 = calc_distance(base_point, base_points[i+1].anchor);
     var orig_dist2 = calc_distance(base_point, base_points[i-1].anchor);
     var orig_dist = Math.min(orig_dist1, orig_dist2);
@@ -62,10 +79,23 @@ function calc_cost(sel_id, i, cur_sel_id, j){
     var dist = calc_distance(base_point, target_point);
     cost += 3.0 * (dist/orig_dist);
     
-    // 進行方向からなす角の内積
-    var dir = calc_dir(base_points[i-1].anchor, base_point);
+    // より近い方の進行方向からなす角の内積
+    // TODO: さらにこの時の微分値を、ハンドルを考慮した正確な方向にする
+    var dir_from_prev = calc_dir(base_points[i-1].anchor, base_point);    // from prev
+    if(has_left_handle(base_points[i])){
+        var left_pos = base_points[i].leftDirection;
+        dir_from_prev = calc_dir(left_pos, base_point);
+    }
+    var dir_from_next = calc_dir(base_points[i+1].anchor, base_point);    // from next
+    if(has_right_handle(base_points[i])){
+        var right_pos = base_points[i].rightDirection;
+        dir_from_next = calc_dir(right_pos, base_point);
+    }
+
     var new_dir = calc_dir(base_point, target_point);
-    cost += 3.0 * (1 - dot(dir, new_dir));
+    var dir_cost = 3.0 * (1 - Math.max(dot(dir_from_prev, new_dir), dot(dir_from_next, new_dir)));
+
+    cost += dir_cost;
 
     // 斜めペナルティ
     var x_axis = Array(1.0, 0.0);
@@ -73,9 +103,9 @@ function calc_cost(sel_id, i, cur_sel_id, j){
     cost += 0.2 * (theta_deg % 90) / 90;
 
     // セレクション跨ぎペナルティ
-    if(sel_id != cur_sel_id){
-        cost += 1.0;
-    }
+    // if(sel_id != cur_sel_id){
+    //     cost += 1.0;
+    // }
 
     return cost;
 }
@@ -189,8 +219,8 @@ for(var sel_id = 0; sel_id < sels.length; sel_id++){
 
         // 全セレクションに対して探索
         var min_cost = 100000000;
-        var min_sel_id = -1;
-        var min_pt_id = -1;
+        var min_sel_id = [-1, -1];
+        var min_pt_id = [-1, -1];
         for(var cur_sel_id = 0; cur_sel_id < sels.length; cur_sel_id++){
             cur_points = sels[cur_sel_id].pathPoints;
             for(var j = 0; j < cur_points.length; j++){
@@ -201,58 +231,82 @@ for(var sel_id = 0; sel_id < sels.length; sel_id++){
                 var cost = calc_cost(sel_id, i, cur_sel_id, j);
 
                 if(cost < min_cost){
+                    var n = 1.3;
+                    // 最小コストのn倍未満であれば2番目にいれていい
+                    if(min_cost < cost*n){
+                        // 0->iにずらす(2番目に小さい)
+                        min_sel_id[1] = min_sel_id[0];
+                        min_pt_id[1] = min_pt_id[0];
+                    }
+                    // n倍以上の差があれば捨てる
+                    else{
+                        min_sel_id[1] = -1;
+                        min_pt_id[1] = -1;
+                    }
+
                     min_cost = cost;
-                    min_sel_id = cur_sel_id;
-                    min_pt_id = j;
+
+                    // // 1番小さい
+                    min_sel_id[0] = cur_sel_id;
+                    min_pt_id[0] = j;
                 }
             }
         }
 
-        // 道なりでなければ線をひく
-        var is_next = min_sel_id == sel_id && min_pt_id == i+1;
-        var is_prev = min_sel_id == sel_id && min_pt_id == i-1;
-        if(!is_next && !is_prev){
-
-            // エッジをまたいでたら引かない
-            var min_point = sels[min_sel_id].pathPoints[min_pt_id].anchor;
-            var line_vec = Array(points[i].anchor, min_point);
-            if(intersect_any(all_edges, line_vec)){
-                // alert("intersected");
-                continue;
+        for(var line_id=0; line_id<2; line_id++){
+            // 1番小さいライン
+            // 道なりでなければ線をひく
+            var is_next = min_sel_id[line_id] == sel_id && min_pt_id[line_id] == i+1;
+            var is_prev = min_sel_id[line_id] == sel_id && min_pt_id[line_id] == i-1;
+            if(!is_next && !is_prev){
+    
+                if(min_sel_id[line_id] == -1){
+                    continue;
+                }
+                if(min_pt_id[line_id] == -1){
+                    continue;
+                }
+    
+                // エッジをまたいでたら引かない
+                var min_point = sels[min_sel_id[line_id]].pathPoints[min_pt_id[line_id]].anchor;
+                var line_vec = Array(points[i].anchor, min_point);
+                if(intersect_any(all_edges, line_vec)){
+                    // alert("intersected");
+                    continue;
+                }
+    
+                // テキストの外部なら引かない
+                if(!is_in_text(all_edges, line_vec)){
+                    // alert("is out the text");
+                    continue;
+                }
+    
+                // インデックスバッファに既にある場合は引かない
+                // if(is_already_exists(indices_buffer, sel_id, i, min_sel_id[line_id], min_pt_id[line_id])){
+                //     alert("is_already_exists");
+                //     continue;
+                // }
+    
+                // add line
+                var line = doc.pathItems.add();
+    
+                // Debug
+                line.stroked = true;
+                var newRGBColor = new RGBColor();
+                newRGBColor.red = 255;
+                newRGBColor.green = 0;
+                newRGBColor.blue = 0;
+                line.strokeColor = newRGBColor;
+                
+                line.setEntirePath(Array(points[i].anchor, min_point));
+    
+                // インデックスバッファに保存
+                indices_buffer.push([sel_id, i, min_sel_id[line_id], min_pt_id[line_id]]);
             }
-
-            // テキストの外部なら引かない
-            if(!is_in_text(all_edges, line_vec)){
-                // alert("is out the text");
-                continue;
-            }
-
-            // インデックスバッファに既にある場合は引かない
-            if(is_already_exists(indices_buffer, sel_id, i, min_sel_id, min_pt_id)){
-                // alert("is_already_exists");
-                continue;
-            }
-
-            // add line
-            var line = doc.pathItems.add();
-
-            // Debug
-            line.stroked = true;
-            var newRGBColor = new RGBColor();
-            newRGBColor.red = 255;
-            newRGBColor.green = 0;
-            newRGBColor.blue = 0;
-            line.strokeColor = newRGBColor;
-            
-            line.setEntirePath(Array(points[i].anchor, min_point));
-
-            // インデックスバッファに保存
-            indices_buffer.push([sel_id, i, min_sel_id, min_pt_id]);
-
-            // line.selected = true;
         }
     }
 }
+
 app.executeMenuCommand('group');
 app.executeMenuCommand("Live Pathfinder Exclude");
 app.executeMenuCommand('expandStyle');
