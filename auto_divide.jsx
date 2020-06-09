61c9d05a485d912d6cd279ecc6331336fc93321e
nishidate-yuki <asta18425@gmail.com>
2020-06-09 09:28:41 +0900

    clean code

--- auto_divide.jsx
+++ auto_divide.jsx
@@ -6,33 +6,29 @@ doc = app.activeDocument;
 sels = doc.selection;
 doc.layers.add();
 
-function sub(a, b){
-    return Array(a[0] - b[0], a[1] - b[1]);
-}
+EPS = 0.0001;
 
-function org_round(value) {
-    return Math.round(value * 10) / 10;
+//-----------------------vec2---------------------------
+function sub(a, b){
+    return [a[0] - b[0], a[1] - b[1]];
 }
 
 function calc_distance(pos1, pos2){
-    var a = Math.pow((pos1[0] - pos2[0]), 2);
-    var b = Math.pow((pos1[1] - pos2[1]), 2);
-    var distance = (Math.sqrt(a + b));
-    return distance;
+    var dx = pos1[0] - pos2[0];
+    var dy = pos1[1] - pos2[1];
+    return Math.sqrt(dx*dx + dy*dy);
 }
 
 function calc_length(pos){
-    var length = Math.sqrt(Math.pow(pos[0], 2) + Math.pow(pos[1], 2));
-    return length;
+    return Math.sqrt(pos[0]*pos[0] + pos[1]*pos[1]);
 }
 
 function normalize(pos){
     var len = calc_length(pos);
-    var norm = Array(pos[0]/len, pos[1]/len);
-    return norm;
+    return [pos[0]/len, pos[1]/len];
 }
 
-function calc_dir(a, b){
+function dir(a, b){
     return normalize(sub(b, a));
 }
 
@@ -40,9 +36,14 @@ function dot(a, b){
     return a[0]*b[0] + a[1]*b[1];
 }
 
+function cross(a, b){
+    return a[0]*b[1] - a[1]*b[0];
+}
+
+
+//-----------------------pathpoint---------------------------
 function has_left_handle(point){
     // ハンドル位置とアンカー位置が異なればハンドルを持つ
-    var EPS = 0.0001;
     var anchor = point.anchor;
     var left = point.leftDirection;
     return Math.abs(anchor[0] - left[0]) > EPS || Math.abs(anchor[1] - left[1]) > EPS;
@@ -50,73 +51,31 @@ function has_left_handle(point){
 
 function has_right_handle(point){
     // ハンドル位置とアンカー位置が異なればハンドルを持つ
-    var EPS = 0.0001;
     var anchor = point.anchor;
     var right = point.rightDirection;
     return Math.abs(anchor[0] - right[0]) > EPS || Math.abs(anchor[1] - right[1]) > EPS;
 }
 
-function calc_cost(sel_id, i, cur_sel_id, j){
 
-    base_points = sels[sel_id].pathPoints;
-    base_point = base_points[i].anchor;
+//-----------------------line---------------------------
+function add_line(pos1, pos2){
+    var line = doc.pathItems.add();
 
-    target_point = sels[cur_sel_id].pathPoints[j].anchor;
+    // stroke
+    line.stroked = true;
+    var newRGBColor = new RGBColor();
+    newRGBColor.red = 255;
+    newRGBColor.green = 0;
+    newRGBColor.blue = 0;
+    line.strokeColor = newRGBColor;
 
-    var cost = 0;
-
-    // 道なりに進まない場合
-    if(sel_id != cur_sel_id || j - i != 1){
-        cost += 0.5;
-    }
-
-    // 距離コスト
-    // 何かしらで正規化が必要 -> 道なりに進む場合を基準値にする
-    // 隣接する辺でより短い方を採用
-    var orig_dist1 = calc_distance(base_point, base_points[i+1].anchor);
-    var orig_dist2 = calc_distance(base_point, base_points[i-1].anchor);
-    var orig_dist = Math.min(orig_dist1, orig_dist2);
-
-    var dist = calc_distance(base_point, target_point);
-    cost += 3.0 * (dist/orig_dist);
-    
-    // より近い方の進行方向からなす角の内積
-    // TODO: さらにこの時の微分値を、ハンドルを考慮した正確な方向にする
-    var dir_from_prev = calc_dir(base_points[i-1].anchor, base_point);    // from prev
-    if(has_left_handle(base_points[i])){
-        var left_pos = base_points[i].leftDirection;
-        dir_from_prev = calc_dir(left_pos, base_point);
-    }
-    var dir_from_next = calc_dir(base_points[i+1].anchor, base_point);    // from next
-    if(has_right_handle(base_points[i])){
-        var right_pos = base_points[i].rightDirection;
-        dir_from_next = calc_dir(right_pos, base_point);
-    }
-
-    var new_dir = calc_dir(base_point, target_point);
-    var dir_cost = 3.0 * (1 - Math.max(dot(dir_from_prev, new_dir), dot(dir_from_next, new_dir)));
-
-    cost += dir_cost;
-
-    // 斜めペナルティ
-    var x_axis = Array(1.0, 0.0);
-    var theta_deg = Math.acos(dot(new_dir, x_axis)) * ( 180 / Math.PI );
-    cost += 0.2 * (theta_deg % 90) / 90;
-
-    // セレクション跨ぎペナルティ
-    // if(sel_id != cur_sel_id){
-    //     cost += 1.0;
-    // }
-
-    return cost;
-}
-
-function cross(a, b){
-    return a[0] * b[1] - a[1] * b[0];
+    line.setEntirePath([pos1, pos2]);
 }
 
 function intersect(edge, line){
-    var EPS = 0.01;
+    // 線分と線分の交差判定
+    // 線分aに対して線分bの頂点が左右両側に存在し、逆も同様であれば交差している
+    // 線分aに対して頂点がどちら側にあるのかは外積の符号で判定できる
     var a1 = edge[0];
     var a2 = edge[1];
     var b1 = line[0];
@@ -142,21 +101,24 @@ function intersect_any(all_edges, line){
 function calc_center(line){
     var pos1 = line[0];
     var pos2 = line[1];
-    var center = Array((pos1[0] + pos2[0])/2, (pos1[1] + pos2[1])/2);
+    var center = [(pos1[0] + pos2[0])/2, (pos1[1] + pos2[1])/2];
     return center;
 }
 
 function is_in_text(all_edges, line){
+    // ラインがテキストの内部にあるかを判定する
+    // ラインの中点から右上45度に線を伸ばし、
+    // それがテキストのエッジと何度交差したかで内外判定を行う
+
     var center = calc_center(line);
-    var right_point = Array(1000000.0, center[1] + 1000000.0);
-    var scanline = Array(center, right_point);
+    var right_point = [1000000.0, center[1] + 1000000.0];
+    var scanline = [center, right_point];
 
     if(scanline == undefined){
         alert("scanline is undefined");
     }
 
     var intersect_cnt = 0;
-
     for(var i = 0; i < all_edges.length; i++){
         if(intersect(all_edges[i], scanline)){
             intersect_cnt++;
@@ -167,7 +129,7 @@ function is_in_text(all_edges, line){
     return intersect_cnt%2 == 1;
 }
 
-function is_already_exists(indices_buffer, sel_id, i, min_sel_id, min_pt_id){
+function exists(indices_buffer, sel_id, i, min_sel_id, min_pt_id){
     for (var buf_id = 0; buf_id < indices_buffer.length; buf_id++){
         // 逆順で既に入っている
         if(indices_buffer[buf_id][0] == min_sel_id
@@ -180,32 +142,75 @@ function is_already_exists(indices_buffer, sel_id, i, min_sel_id, min_pt_id){
     return false;
 }
 
-function add_line(pos1, pos2){
-    var line = doc.pathItems.add();
 
-    // stroke
-    line.stroked = true;
-    var newRGBColor = new RGBColor();
-    newRGBColor.red = 255;
-    newRGBColor.green = 0;
-    newRGBColor.blue = 0;
-    line.strokeColor = newRGBColor;
 
-    line.setEntirePath(Array(pos1, pos2));
+//-----------------------cost---------------------------
+function calc_cost(sel_id, i, cur_sel_id, j){
+    // スクリプトのメインとなるコスト関数
+    var cost = 0;
+
+    base_points = sels[sel_id].pathPoints;
+    base_point = base_points[i].anchor;
+    target_point = sels[cur_sel_id].pathPoints[j].anchor;
+
+    // 道なりに進まないコスト
+    if(sel_id != cur_sel_id || j - i != 1){
+        cost += 0.5;
+    }
+
+    // 距離コスト
+    // 何かしらで正規化が必要 -> 隣接する辺でより短い方を基準値とする
+    var orig_dist1 = calc_distance(base_point, base_points[i+1].anchor);
+    var orig_dist2 = calc_distance(base_point, base_points[i-1].anchor);
+    var orig_dist = Math.min(orig_dist1, orig_dist2);
+
+    var dist = calc_distance(base_point, target_point);
+    cost += 3.0 * (dist/orig_dist);
+    
+    // 方向コスト
+    // ポイントの左右の方向ベクトルを計算
+    var dir_from_prev = dir(base_points[i-1].anchor, base_point);
+    if(has_left_handle(base_points[i])){  // ハンドルを持っている場合はベクトルを変更
+        var left_pos = base_points[i].leftDirection;
+        dir_from_prev = dir(left_pos, base_point);
+    }
+    var dir_from_next = dir(base_points[i+1].anchor, base_point);
+    if(has_right_handle(base_points[i])){  // ハンドルを持っている場合はベクトルを変更
+        var right_pos = base_points[i].rightDirection;
+        dir_from_next = dir(right_pos, base_point);
+    }
+    // target->baseのベクトルが左右(のより近い方)からどれだけずれているか
+    var new_dir = dir(base_point, target_point);
+    cost += 3.0 * (1 - Math.max(dot(dir_from_prev, new_dir), dot(dir_from_next, new_dir)));
+
+
+    // 斜めに進むコスト
+    var x_axis = [1.0, 0.0];
+    var theta_deg = Math.acos(dot(new_dir, x_axis)) * ( 180 / Math.PI );
+    cost += 0.2 * (theta_deg % 90) / 90;
+
+    // セレクション跨ぎコスト
+    // if(sel_id != cur_sel_id){
+    //     cost += 1.0;
+    // }
+
+    return cost;
 }
+
+
 
 //-----------------------main---------------------------
 
-// calc all edges
+// 選択されたテキストが含む全ての辺を計算しておく
 var all_edges = []
 for(var sel_id = 0; sel_id < sels.length; sel_id++){
     points = sels[sel_id].pathPoints;
     for (var i = 0; i < points.length; i++){
         var edge;
         if(i == points.length-1){
-            edge = Array(points[i].anchor, points[0].anchor);
+            edge = [points[i].anchor, points[0].anchor];
         }else{
-            edge = Array(points[i].anchor, points[i+1].anchor);
+            edge = [points[i].anchor, points[i+1].anchor];
         }
         all_edges.push(edge);
     }
@@ -287,7 +292,7 @@ for(var sel_id = 0; sel_id < sels.length; sel_id++){
     
                 // エッジをまたいでたら引かない
                 var min_point = sels[min_sel_id[line_id]].pathPoints[min_pt_id[line_id]].anchor;
-                var line_vec = Array(points[i].anchor, min_point);
+                var line_vec = [points[i].anchor, min_point];
                 if(intersect_any(all_edges, line_vec)){
                     continue;
                 }
@@ -298,8 +303,8 @@ for(var sel_id = 0; sel_id < sels.length; sel_id++){
                 }
     
                 // インデックスバッファに既にある場合は引かない
-                // if(is_already_exists(indices_buffer, sel_id, i, min_sel_id[line_id], min_pt_id[line_id])){
-                //     alert("is_already_exists");
+                // if(exists(indices_buffer, sel_id, i, min_sel_id[line_id], min_pt_id[line_id])){
+                //     alert("exists");
                 //     continue;
                 // }
 
